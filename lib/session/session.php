1d1273ac8f9aae9050c1552ab2069f29ec88d5d1
erratic <erratic@shells.yourstruly.sx>
2018-08-18 21:23:52 +0000

    fixed session class

--- lib/session/session.php
+++ lib/session/session.php
@@ -39,15 +39,21 @@ class session
         $this->cookies = $_COOKIE;
         $this->backend = new $this->settings->type();
 
-        if(!in_array("pscr_session_id", $this->cookies))
+        if(!array_key_exists("pscr_session_id", $this->cookies))
         {
+            logger::_()->info($this, $this->cookies);
             $this->cookies = array("pscr_session_id" => hash('sha256', rand() . time()));
             logger::_()->info($this, "new session", $this->cookies);
+        }
+        else
+        {
+            logger::_()->info($this, "session already exists", $this->cookies['pscr_session_id']);
         }
     }
 
     function send_cookies_to_client() {
         foreach($this->cookies as $key => $value) {
+            logger::_()->info($this, "sending existing cookie", $key, $value);
             setcookie($key, $value, time()+3600);
         }
     }
