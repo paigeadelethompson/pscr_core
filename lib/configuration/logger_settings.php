89721da1fcd88810cb60ea10d67d954ba330c310
Paige Thompson <paigeadele@gmail.com>
2022-07-31 16:00:34 -0700

    Update logger_settings.php

--- lib/configuration/logger_settings.php
+++ lib/configuration/logger_settings.php
@@ -53,7 +53,7 @@ class logger_settings extends pscr_settings {
         $section = get_called_class();
         $data = parse_ini_file(PSCR_PROJECT_ROOT . './settings/settings.ini', true);
 
-        if(array_key_exists($section, $data)) {
+        if(property_exists($section, $data)) {
             $this->data = new \ArrayObject($data[$section], \ArrayObject::ARRAY_AS_PROPS);
         }
         else {
